7fde6127546cff444381b29bfa133cb1fc9356ba
Yermek <tisobyn@gmail.com>
2020-08-18 01:24:33 +0600

    Start
    first created app

---
+++ HotelApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HotelApp
+//
+//  Created by Yermek Sabyrzhan on 8/18/20.
+//  Copyright © 2020 Yermek Sabyrzhan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
